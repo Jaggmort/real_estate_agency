5a0cb62a4bc69bae57817a8afc30d9a201be6e78
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-03-31 13:47:00 +0300

    Added inline show of flat owners

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Flat, Complain, Owner
 
+
+class OwnerInline(admin.TabularInline):
+    model = Owner.own_flats.through
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
     readonly_fields = ['created_at']
@@ -8,6 +12,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['liked_by']
+    inlines = [OwnerInline]
 
 class ComplainAdmin(admin.ModelAdmin):
     raw_id_fields = ['user', 'complain_flat']
@@ -15,7 +20,6 @@ class ComplainAdmin(admin.ModelAdmin):
 class OwnerAdmin(admin.ModelAdmin):
     list_display=['owner', 'owners_phonenumber', 'owner_pure_phone']
     raw_id_fields=['own_flats']
-
 
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complain, ComplainAdmin)
