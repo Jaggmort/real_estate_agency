9314adf9dd46e6fb9a034b2b890378b9ea680c30
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-03-31 12:45:38 +0300

    Filled Owners

--- property/models.py
+++ property/models.py
@@ -63,4 +63,4 @@ class Owner(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True, null=True)
-    own_flats = models.ManyToManyField(Flat, related_name='owned_flats', null=True)
+    own_flats = models.ManyToManyField(Flat, related_name='owned_flats', null=True, blank=True)
