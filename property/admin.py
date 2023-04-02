20bc5370e0ea571636bd2044abe546f3fbed74de
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-04-02 11:52:36 +0300

    Started use of @register

--- property/admin.py
+++ property/admin.py
@@ -6,6 +6,7 @@ class OwnerInline(admin.TabularInline):
     model = Owner.own_flats.through
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
     readonly_fields = ['created_at']
@@ -22,14 +23,12 @@ class FlatAdmin(admin.ModelAdmin):
     inlines = [OwnerInline]
 
 
+@admin.register(Complain)
 class ComplainAdmin(admin.ModelAdmin):
     raw_id_fields = ['user', 'complain_flat']
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     list_display = ['owner', 'owners_phonenumber', 'owner_pure_phone']
     raw_id_fields = ['own_flats']
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complain, ComplainAdmin)
-admin.site.register(Owner, OwnerAdmin)
