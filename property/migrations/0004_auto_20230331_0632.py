f2a7f37bf7285154f7edcc769e615598999cc5ae
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-04-04 03:33:08 +0300

    Changed *_0632 migration

--- property/migrations/0004_auto_20230331_0632.py
+++ property/migrations/0004_auto_20230331_0632.py
@@ -5,11 +5,7 @@ from django.db import migrations
 
 def fill_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
-        flat.new_building = False
-        if flat.construction_year >= 2015:
-            flat.new_building = True
-        flat.save()
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
 
 class Migration(migrations.Migration):
 
